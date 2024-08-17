be5eb4f73201f3c45c641a068f0423001d51c071
HossamAltokhy <hossam.abdulghany853@gmail.com>
2024-08-17 17:49:41 +0300

    the start point

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   main.c
+ * Author: hossa
+ *
+ * Created on August 17, 2024, 4:30 PM
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
