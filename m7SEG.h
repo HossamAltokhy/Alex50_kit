3e4f2ab6af5cb9744d3c36703e0f2366eaaee6b8
HossamAltokhy <hossam.abdulghany853@gmail.com>
2024-08-30 17:22:17 +0300

    7 segment Code

--- m7SEG.h
+++ m7SEG.h
@@ -8,16 +8,27 @@
 #ifndef M7SEG_H
 #define	M7SEG_H
 
-#ifdef	__cplusplus
-extern "C" {
-#endif
 
 
 
+#define _7SEG_PORT_DIR    DDRA
+#define _7SEG_PORT_DATA   PORTA
+#define _7SEG_EN_DIR      DDRB
+#define _7SEG_EN          PORTB
+#define _7SEG_DIP_DIR     DDRB
+#define _7SEG_DIP         PORTB
 
-#ifdef	__cplusplus
-}
-#endif
+#define _7SEG_R_EN        PB1     
+#define _7SEG_L_EN        PB2     
+
+
+void init_7SEG();
+void set_7SEG_L(int num);
+void set_7SEG_R(int num);
+void set_7SEG_DIP(char state);
+
+
+
 
 #endif	/* M7SEG_H */
 
--- main.c
+++ main.c
@@ -15,23 +15,9 @@
 #include "mBUTTON.h"
 #include "mRELAY.h"
 #include "mKeypad.h"
+#include "m7SEG.h"
 
 
-#define _7SEG_PORT_DIR    DDRA
-#define _7SEG_PORT_DATA   PORTA
-#define _7SEG_EN_DIR      DDRB
-#define _7SEG_EN          PORTB
-#define _7SEG_DIP_DIR     DDRB
-#define _7SEG_DIP         PORTB
-
-#define _7SEG_R_EN        PB1     
-#define _7SEG_L_EN        PB2     
-
-
-void init_7SEG();
-void set_7SEG_L(int num);
-void set_7SEG_R(int num);
-void set_7SEG_DIP(char state);
 
 
 
@@ -51,40 +37,3 @@ int main(void) {
     }
 }
 
-void init_7SEG(){
-    
-    
-    _7SEG_PORT_DIR |= (1<<7)|(1<<6)|(1<<5)|(1<<4);
-    _7SEG_DIP_DIR |= (1<<3);
-    _7SEG_EN_DIR |= (1<<2)|(1<<1);
-    
-    //_7SEG_EN |= (1<<2)|(1<<1);
-}
-
-void set_7SEG_L(int num){
-    _7SEG_EN |= (1<<_7SEG_L_EN);
-    _7SEG_PORT_DATA &= 0x0F; //_7SEG_PORT_DATA &= ~((1<<7)|(1<<6)|(1<<5)|(1<<4));
-    _7SEG_PORT_DATA |= (num<<4);
-    set_7SEG_DIP(ON);
-    _delay_ms(1);
-    _7SEG_EN &= ~(1<<_7SEG_L_EN);
-}
-
-void set_7SEG_R(int num){
-    _7SEG_EN |= (1<<_7SEG_R_EN);
-    _7SEG_PORT_DATA &= 0x0F; //_7SEG_PORT_DATA &= ~((1<<7)|(1<<6)|(1<<5)|(1<<4));
-    _7SEG_PORT_DATA |= (num<<4);
-    _delay_ms(1);
-    _7SEG_EN &= ~(1<<_7SEG_R_EN);
-}
-
-void set_7SEG_DIP(char state){
-    switch(state){
-        case ON:
-            _7SEG_DIP |= (1<<3);
-            break;
-        case OFF:
-            _7SEG_DIP &= ~(1<<3);
-            break;
-    }
-}
